c70d25c3910e469eeef305a489b7e90eef809817
axai <02b3akshay@gmail.com>
2023-08-06 16:32:56 +0530

    Add even  filter out function

--- loops.js
+++ loops.js
@@ -1,8 +1,12 @@
 /** @format */
 
-for (let i = 10; i >= 1; i = i - 1) {
-	if (i == 5) {
-		continue;
+function doSomething(arr) {
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i] % 2 == 0) {
+			console.log(arr[i]);
+		}
 	}
-	console.log(i);
 }
+
+var arr = [100, 21, 56, 46, 12, 8, 1, 3];
+doSomething(arr);
