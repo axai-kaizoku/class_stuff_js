cf21d56f3b190fcf94560c94433afa7b345508b6
axai <02b3akshay@gmail.com>
2023-08-06 17:20:46 +0530

    Add using all types of loops and desicion making

--- loops.js
+++ loops.js
@@ -1,8 +1,30 @@
 /** @format */
 
-for (let i = 1; i <= 10; i++) {
-	for (let j = 1; j <= 10; j++) {
-		console.log(i + ' x ' + j + ' = ' + i * j);
+let choice = -1;
+
+do {
+	choice = parseInt(
+		prompt(
+			'Pick an item : \n1. Tea\n2. Coffee\n3. Softdrinks\n4. Water\n5. Exit'
+		)
+	);
+	switch (choice) {
+		case 1:
+			alert('Tea is coming up!');
+			break;
+		case 2:
+			alert('Coffee is coming up!');
+			break;
+		case 3:
+			alert('Softdrink is coming up!');
+			break;
+		case 4:
+			alert('Water is coming up!');
+			break;
+		case 5:
+			alert('Thankyou for shopping by');
+			break;
+		default:
+			alert('Invalid choice');
 	}
-	console.log('\n');
-}
+} while (choice >= 1 && choice <= 4);
