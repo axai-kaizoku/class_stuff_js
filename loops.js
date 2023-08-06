5b354cb3d591a9c61e58e5ba36bc5c2511179171
axai <02b3akshay@gmail.com>
2023-08-06 16:35:26 +0530

    Add Find the sum of the first 10 numbers

--- loops.js
+++ loops.js
@@ -1,12 +1,10 @@
 /** @format */
 
-function doSomething(arr) {
-	for (var i = 0; i < arr.length; i++) {
-		if (arr[i] % 2 == 0) {
-			console.log(arr[i]);
-		}
-	}
+let sum = 0;
+for (let j=1; j<=10; j++)
+{
+  console.log('Loop no.'+ j)
+  sum=sum+j
 }
 
-var arr = [100, 21, 56, 46, 12, 8, 1, 3];
-doSomething(arr);
+console.log(`The sum of first ten numbers is : ${sum}`)
