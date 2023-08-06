7f771c50b109641e1a2e3f3a9e8a650276eeadf5
axai <02b3akshay@gmail.com>
2023-08-06 16:37:05 +0530

    Add Displaying each character of a word on a new line

--- loops.js
+++ loops.js
@@ -1,10 +1,8 @@
 /** @format */
 
-let sum = 0;
-for (let j=1; j<=10; j++)
-{
-  console.log('Loop no.'+ j)
-  sum=sum+j
+'use strict'
+const word = 'RESPECT';
+for (const w of word){
+  console.log(w)
 }
-
-console.log(`The sum of first ten numbers is : ${sum}`)
+console.log(`Find out what it means to me...`)
