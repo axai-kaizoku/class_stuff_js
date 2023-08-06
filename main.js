0e414034a1a9ef7c83cfdcdc7e2359479ff830db
axai <02b3akshay@gmail.com>
2023-08-06 19:08:30 +0530

    Add an activity (functions and call methods)

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
 /** @format */
 
-function tax(price, taxRate) {
-	return(parseInt(price + (taxRate / 100) * price));
-}
+let orderData1 = {
+	name: 'Shampoo',
+	cost: 500,
+	discount: 10,
+	taxRate: 10,
+	inStock: true,
+	calculatePrice: function () {
+		return `The total price was ${(this.cost + (this.taxRate * 100) / this.cost - (this.discount * 100) / this.cost).toFixed(2)}`;
+	},
+	displayInstock: function () {
+		if (this.inStock) {
+			return this.name + ' is in stock!';
+		} else {
+			return this.name + ' is not in stock!';
+		}
+	},
+	showDiscount: function () {
+		return 'The total discount applied was :' + this.discount;
+	},
+};
 
-console.log("The total price including tax is ",tax(100, 10));
+console.log(orderData1.calculatePrice());
+console.log(orderData1.displayInstock());
+console.log(orderData1.showDiscount());
+
