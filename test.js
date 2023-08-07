180e989f8eec3b657fd77b32337275661c628146
axai <02b3akshay@gmail.com>
2023-08-07 22:44:05 +0530

    Add an activity ( this is a purely luck based program)

--- test.js
+++ test.js
@@ -1,15 +1,26 @@
-let temperature = function (temp, unit='F') {
-  if (unit == 'F') {
-    let celcius;
-    celcius = (temp - 32) * 5 / 9;
-    return ` Temperature : ${celcius}\u{00B0}C`;
-  } else if (unit == 'C') {
-      let fahrenheit;
-      fahrenheit = (temp * 9 / 5) + 32;
-      return ` Temperature : ${fahrenheit}\u{00B0}F`;
-  } else {
-      return `Invalid unit ${unit}`;
-  }
-}
+/** @format */
 
-console.log(temperature(15, 'C'));
+// This is based on pure luck.
+// We can't find the correct in low inputs.
+// If these commented alert messages are not there it is almost impossible.
+
+const randomLuck = function () {
+	const randNum1 = Math.floor(1 + Math.random() * 90);
+  const randNum = Number(randNum1)
+	let input;
+	while (randNum != input) {
+		input = prompt('Enter a number');
+    // alert(randNum-5);
+		if (randNum == input) {
+			alert `Congratulations!`;
+			break;
+		} else if (randNum > input) {
+			alert `Too Low!`;
+		} else if (randNum < input) {
+			alert `Too High!`;
+		}
+    // alert(randNum+5);
+	}
+};
+
+randomLuck();
