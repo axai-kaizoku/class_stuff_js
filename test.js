8f832249dc54c61ac3febe303cdd7901d345ddd3
axai <02b3akshay@gmail.com>
2023-08-12 15:07:54 +0530

    Add isPalindrome()

--- test.js
+++ test.js
@@ -1,12 +1,5 @@
 /** @format */
 
-const books = [
-	'The Song of Ice and Fire',
-	'Deception',
-	'The subtle art of not giving a fuck',
-	'Ikegai',
-];
+const isPalindrome = (word) => word.split('').reverse().join('') === word;
 
-const disp = function () {books.forEach((elem, index) => console.log(elem.split(' ').reverse().join(' ')))}
-
-disp();
+console.log(isPalindrome('tenet'));
