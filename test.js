53d54d3bf62ad24568ec9b9729c18eee490e875c
axai <02b3akshay@gmail.com>
2023-08-12 15:44:19 +0530

    Add arrays activity

--- test.js
+++
@@ -1,8 +0,0 @@
-/** @format */
-
-const desk = ['Laptop', 'Mouse', 'Speakers', 'Monitor', 'Microphone'];
-
-console.log(desk.slice(2));
-console.log(desk.slice(1, 3));
-console.log(desk.slice(4));
-console.log(desk.slice(3));
