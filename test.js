01897699773092ac4c315881f60a137479c43145
axai <02b3akshay@gmail.com>
2023-08-12 14:29:16 +0530

    Add array datatype object

--- test.js
+++ test.js
@@ -1,13 +1,4 @@
-function findMax () {
-  let arr = [1, 2, 3, 4, 5]
-  let res;
-  for (let i of arr) {
-      if (arr[i] > arr[i+1]) {
-          res = arr[i];
-          console.log(arr[i])
-      }
-      console.log(res);
-  }
-}
+/** @format */
 
-findMax();
+let bookTitles = ['The Book Thief', 'Angels and Demons', '1984'];
+console.log(bookTitles[1]);
