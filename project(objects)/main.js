a300611ca82d51097b3cdd96678b6bcba2c31ad1
axai <02b3akshay@gmail.com>
2023-08-12 16:37:47 +0530

    Setup and understand the problem

---
+++ project(objects)/main.js
@@ -0,0 +1,27 @@
+let allBooks = [
+	{
+		id: 1,
+		title: 'Angles and Demons',
+		author: 'Dan Brown',
+		ratings: [3.0, 3.5, 4.2, 4.6],
+		reviews: [
+			{
+				username: 'John',
+				content:
+					'		Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, odit.Enim totam, recusandae, qui aliquid pariatur fugit est magni esse eaquedistinctio placeat laudantium voluptatibus. Perspiciatis ex nobis hic libero!',
+			},
+			{
+				username: 'Jane',
+				content:
+					'		Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, odit.Enim totam, recusandae, qui aliquid pariatur fugit est magni esse eaquedistinctio placeat laudantium voluptatibus. Perspiciatis ex nobis hic libero!',
+			},
+			{
+				username: 'Bob',
+				content:
+					'		Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, odit.Enim totam, recusandae, qui aliquid pariatur fugit est magni esse eaquedistinctio placeat laudantium voluptatibus. Perspiciatis ex nobis hic libero!',
+			},
+		],
+	},
+];
+
+let book;
