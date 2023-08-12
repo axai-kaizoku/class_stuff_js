ffcced5391f91caf5d3b9fa3ad50c21a29a95136
axai <02b3akshay@gmail.com>
2023-08-12 14:58:38 +0530

    Add reverse()

--- test.js
+++ test.js
@@ -1,30 +1,3 @@
-/** @format */
+const productReviews = ['Review One', 'Review Two', 'Review Three', 'Review Four']
 
-function groupArrayElements(inputArr, n) {
-	// Check if n is a valid number
-	const result = [];
-	if (n <= 0 || !Number.isInteger(n)) {
-		return result;
-	}
-
-	// calculate the length of each subarray
-	const len = Math.floor(inputArr.length / n);
-
-	// initialise the array
-	let start = 0;
-	let end = len;
-
-	for (let i = 0; i < n; i++) {
-		if (i === n - 1) {
-			result.push(inputArr.slice(start));
-		} else {
-			result.push(inputArr.slice(start, end));
-		}
-		start = end;
-		end += len;
-	}
-
-	return result;
-}
-
-console.log(groupArrayElements([1, 2, 3, 4, 5, 6, 7, 8], 6));
+console.log(productReviews.reverse())
