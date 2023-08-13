f9d7b540a51a7ce9ebeb68b6b18ffde80ead5e53
axai <02b3akshay@gmail.com>
2023-08-13 16:07:22 +0530

    Add event-booking project & setup

---
+++ project_DOM/event-booking/src/js/index.js
@@ -0,0 +1,32 @@
+allEvents = [
+	{
+		id: 1,
+		name: 'Birthday',
+		seats: 10,
+		image: './images/birthday.jpg',
+	},
+	{
+		id: 2,
+		name: 'Buffet Breakfast',
+		seats: 6,
+		image: './images/buffet_breakfast.jpg',
+	},
+	{
+		id: 3,
+		name: 'Movie',
+		seats: 10,
+		image: './images/movie.jpg',
+	},
+	{
+		id: 4,
+		name: 'Buffet Lunch',
+		seats: 10,
+		image: './images/buffet_lunch.jpg',
+	},
+	{
+		id: 5,
+		name: 'Buffet Dinner',
+		seats: 20,
+		image: './images/buffet_dinner.jpg',
+	},
+];
