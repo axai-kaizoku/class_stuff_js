285aae1eb99417e3f31058ab03a5f33cdd2813a3
axai <02b3akshay@gmail.com>
2023-08-13 18:43:37 +0530

    Add Project completed

--- project_DOM/event-booking/src/js/index.js
+++ project_DOM/event-booking/src/js/index.js
@@ -1,4 +1,4 @@
-allEvents = [
+let allEvents = [
 	{
 		id: 1,
 		name: 'Birthday',
@@ -14,13 +14,13 @@ allEvents = [
 	{
 		id: 3,
 		name: 'Movie',
-		seats: 10,
+		seats: 12,
 		image: './images/movie.jpg',
 	},
 	{
 		id: 4,
 		name: 'Buffet Lunch',
-		seats: 10,
+		seats: 0,
 		image: './images/buffet_lunch.jpg',
 	},
 	{
@@ -30,3 +30,36 @@ allEvents = [
 		image: './images/buffet_dinner.jpg',
 	},
 ];
+
+let content = ``;
+
+allEvents.forEach((event) => {
+	event = `<div class="event" style="background-image:url('${event.image}')">
+  <h3>${event.name}</h3>
+  <p>Seats left : ${event.seats}</p>
+  <h4>  </h4>
+  <button id=${event.id}>Book Now</button>
+</div>`;
+
+	content += event;
+});
+
+document.getElementById('allEvents').innerHTML = content;
+
+let allButtons = document.querySelectorAll('button');
+
+allButtons.forEach((button) => {
+	button.addEventListener('click', () => checkAvaliability(button));
+});
+
+function checkAvaliability(button) {
+	let buttonID = button.id;
+	let totalSeats = allEvents[buttonID - 1].seats;
+
+	let availablealert = document.querySelectorAll('h4')[buttonID - 1];
+	availablealert.style.display = 'block';
+
+	totalSeats == 0
+		? (availablealert.innerText = 'Not Available')
+		: (availablealert.innerText = 'Available');
+}
