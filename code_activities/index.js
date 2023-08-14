53ebf147d71c627cb1765da92d519480b64e4f7a
axai <02b3akshay@gmail.com>
2023-08-14 18:30:53 +0530

    Add a w8 activity (return even numbers in an array)

--- code_activities/index.js
+++ code_activities/index.js
@@ -1,20 +1,8 @@
-function Book(title, author, year) {
-	this.title = title;
-	this.author = author;
-	this.year = year;
-}
+const evenNum = function (arr) {
+	let finalarr = [];
+	arr.forEach((elem) => (elem % 2 === 0 ? finalarr.push(elem) : elem));
+	return finalarr;
+};
 
-let book1 = new Book('The Song of Ice and Fire', 'Gorge RR Martin', 1989);
-
-let book2 = new Book('Ikegai', 'Momonouske', 2015);
-
-let book3 = new Book('Do It Today', 'Darius', 2000);
-
-let bookCollection = [];
-bookCollection.push(book1, book2, book3);
-
-bookCollection.forEach((book) =>
-	console.log(
-		`Title : ${book.title}, Author : ${book.author}, Year : ${book.year}`,
-	),
-);
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(evenNum(array));
