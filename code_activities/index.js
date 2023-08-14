c831013343730477e314c895f50e0239f12015c0
axai <02b3akshay@gmail.com>
2023-08-14 19:33:40 +0530

    Add a w8 activity (removeDup(removes duplicates from original array and return new array with old array order))

--- code_activities/index.js
+++ code_activities/index.js
@@ -1,36 +1,22 @@
-function countByType(arr) {
-	let typeCount = {};
-	for (let item of arr) {
-		let type = typeof item;
-
-		// Special case for null, since typeof null returns "object"
-		// if (item === null) {
-		// 	type = 'null';
-		// }
-
-		// Increment the count for the determined data type in the typeCount object
-		if (typeCount[type]) {
-			typeCount[type]++;
-		} else {
-			typeCount[type] = 1;
+function removeDup(arr) {
+	let newArr = [];
+	for (let string of arr) {
+		if (!newArr.includes(string)) {
+			newArr.push(string);
 		}
 	}
-
-	return typeCount;
+	return newArr;
 }
 
-const mixedArr = [
-	1,
-	2,
-	'a',
-	'b',
-	true,
-	false,
-	null,
-	undefined,
-	{},
-	[],
-	() => {},
+let array = [
+	'apple',
+	'banana',
+	'orange',
+	'apple',
+	'pear',
+	'banana',
+	'kiwi',
+	'kiwi',
 ];
 
-console.log(countByType(mixedArr));
+console.log(removeDup(array));
