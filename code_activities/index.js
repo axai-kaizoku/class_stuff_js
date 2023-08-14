a8e3e7b51cdd4e4b5e4b5a0b3086b63ed109149e
axai <02b3akshay@gmail.com>
2023-08-14 20:11:10 +0530

    Add a w8 activity (age(calculates youngest and oldest return its name))

--- code_activities/index.js
+++ code_activities/index.js
@@ -1,22 +1,32 @@
-function removeDup(arr) {
-	let newArr = [];
-	for (let string of arr) {
-		if (!newArr.includes(string)) {
-			newArr.push(string);
+function age(arr) {
+	let sum = 0;
+	let youngestStudent = arr[0];
+	let oldestStudent = arr[0];
+
+	for (let elem of arr) {
+		sum += elem.age;
+
+		if (elem.age < youngestStudent.age) {
+			youngestStudent = elem;
+		}
+		if (elem.age > oldestStudent.age) {
+			oldestStudent = elem;
 		}
 	}
-	return newArr;
+
+	let avg = sum / arr.length;
+
+	return `{
+    avgAge: ${avg},
+    oldest: ${oldestStudent.name}
+    youngest: ${youngestStudent.name}
+  }`;
 }
 
-let array = [
-	'apple',
-	'banana',
-	'orange',
-	'apple',
-	'pear',
-	'banana',
-	'kiwi',
-	'kiwi',
+const sampleData = [
+	{ name: 'John', age: 25 },
+	{ name: 'Mary', age: 30 },
+	{ name: 'Peter', age: 20 },
+	{ name: 'Sarah', age: 28 },
 ];
-
-console.log(removeDup(array));
+console.log(age(sampleData));
