842a64fbb2fec2df0a8d8922db5048c5c4e73c9d
axai <02b3akshay@gmail.com>
2023-08-19 22:35:59 +0530

    Add insta like button counter

--- test.js
+++ test.js
@@ -1,5 +1,32 @@
-document.getElementById('subscribe').addEventListener('click', () => {
-	alert(
-		`You have subscribed to our page\n\nGet exciting offers and deals on your interest\nSubmit your mail!`,
-	);
+// Define the post content
+let post = `
+<div class="post">
+    <img
+        src="https://picsum.photos/400/300"
+        alt="picsum pic" />
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+    <button id="likeBtn" style="cursor: pointer">Like</button>
+		<span id="likeCount">0</span> Likes
+</div>`;
+
+// // create a random number of posts
+// let randNum = Math.floor(2 + Math.random() * 4);
+// console.log(randNum);
+// for (let i = 0; i < randNum; i++) {
+// 	post += post;
+// 	console.log(post);
+// 	console.log(randNum);
+// }
+
+// Get the postsDiv and set its innerHTML
+const postsDiv = document.getElementById('posts');
+postsDiv.innerHTML = post;
+
+const likeBtn = document.getElementById('likeBtn');
+const likeCountSpan = document.getElementById('likeCount');
+let count = 0;
+
+likeBtn.addEventListener('click', () => {
+	count += 1;
+	likeCountSpan.innerText = count; // Update the count in the span
 });
