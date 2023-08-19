62e2f3962201653f040759f896e2fc054010e5c6
axai <02b3akshay@gmail.com>
2023-08-19 21:28:06 +0530

    Add newsletter button eventlistener activity

--- test.js
+++ test.js
@@ -1,21 +1,5 @@
-let saveBtn = document.getElementById('saveBtn');
-saveBtn.addEventListener('click', storePost);
-
-function storePost() {
-	// Reading the <input> value
-	let blogTitle = document.getElementById('blogTitle').value;
-	// New <li> tag created
-	let newPostItem = document.createElement('li');
-	// New <p> tag created
-	let newPostContent = document.createElement('p');
-	// Adding <input> to <p>
-	newPostContent.innerText = blogTitle;
-	// Adding <p> to <li>
-	newPostItem.appendChild(newPostContent);
-	// Adding all Posts list
-	let allPosts = document.getElementById('allPosts');
-	// Adding <li> to <ul>
-	allPosts.appendChild(newPostItem);
-	// Clearing the <input> field
-	document.getElementById('blogTitle').value = '';
-}
+document.getElementById('subscribe').addEventListener('click', () => {
+	alert(
+		`You have subscribed to our page\n\nGet exciting offers and deals on your interest\nSubmit your mail!`,
+	);
+});
