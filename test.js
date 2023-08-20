5bd423665123c0d3342beb8b039649e42b929a5c
axai <02b3akshay@gmail.com>
2023-08-20 22:26:27 +0530

    Add Inheritance and Encapsulation activity

--- test.js
+++ test.js
@@ -1,28 +1,39 @@
-class Person {
-	constructor(name, id, city) {
-		this.name = name;
-		this.id = id;
-		this.city = city;
+class Vehicle {
+	constructor(manufacturer, model, year) {
+		this.manufacturer = manufacturer;
+		this.model = model;
+		this.year = year;
 	}
-	showInfo() {
-		return `${this.name} lives in ${this.city}`;
-	}
-}
-
-class Librarian extends Person {}
-
-class Student extends Person {
-	constructor(name, id, city, unv) {
-		super(name, id, city);
-		this.unv = unv;
-	}
-
-	showStudentInfo() {
-		return `${this.name} lives in ${this.city} and studies in ${this.unv} and has the student id of ${this.id}`;
+	getInfo() {
+		return `
+		Manufacturer : ${this.manufacturer}
+		Model : ${this.model}
+		Released Year : ${this.year}
+		`;
 	}
 }
 
-let newStudent = new Student('Akshay', '3', 'Hyd', 'University of Illunoi');
-console.log(newStudent.showStudentInfo());
-let newLibrarian = new Librarian('Jane', 2, 'Hyd');
-console.log(newLibrarian.showInfo());
+class Car extends Vehicle {
+	constructor(manufacturer, model, year, fuelType) {
+		super(manufacturer, model, year);
+		this.fuelType = fuelType;
+	}
+
+	getInfo() {
+		return `
+		Manufacturer : ${this.manufacturer}
+		Model : ${this.model}
+		Released Year : ${this.year}
+		Fuel Type : ${this.fuelType}
+		`;
+	}
+}
+
+const vehicle = new Vehicle('Volvo', 'TX-900', 2014);
+console.log(vehicle);
+
+const polo = new Car('Volkwagen', 'DTI-SPORT', 2020, 'Diesel');
+const i20 = new Car('Hyundai', 'MODY', 2019, 'Diesel');
+
+console.log(polo);
+console.log(i20);
