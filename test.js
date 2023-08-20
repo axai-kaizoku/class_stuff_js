7d16aad23d35af4a6b4ec6d6b7794f6e17080501
axai <02b3akshay@gmail.com>
2023-08-20 20:53:38 +0530

    Add higher order functions example

--- test.js
+++ test.js
@@ -1,6 +1,26 @@
-function multiplyByY(num) {
-	let y = 20;
-	return `${y * num}`;
+function compute(x, y, fn) {
+	if (typeof x === 'number' && typeof y === 'number') {
+		return () => fn(x, y);
+	}
+	return () => null;
 }
 
-console.log(multiplyByY(3));
+function multiply(x, y) {
+	return x * y;
+}
+
+function add(x, y) {
+	return x + y;
+}
+
+function substract(a, b) {
+	return a - b;
+}
+
+let sub = compute(40, 20, substract);
+let multpy = compute(10, 20, multiply);
+let ad = compute(10, 20, add);
+
+console.log(sub());
+console.log(ad());
+console.log(multpy());
