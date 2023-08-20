9f5f8fcb9dd7c356e55a6bb08bc678312062eb9f
axai <02b3akshay@gmail.com>
2023-08-20 21:47:20 +0530

    Add Inheritance-0

--- test.js
+++ test.js
@@ -1,14 +1,28 @@
-function cartTotal() {
-	let item = 0;
-	return (newItem) => {
-		item += newItem;
-		return item;
-	};
+class Person {
+	constructor(name, id, city) {
+		this.name = name;
+		this.id = id;
+		this.city = city;
+	}
+	showInfo() {
+		return `${this.name} lives in ${this.city}`;
+	}
 }
 
-const cart = cartTotal();
-console.log(cartTotal()(100));
-console.log(`-------`);
-console.log(cart(110));
-console.log(cart(800));
-console.log(cart(880));
+class Librarian extends Person {}
+
+class Student extends Person {
+	constructor(name, id, city, unv) {
+		super(name, id, city);
+		this.unv = unv;
+	}
+
+	showStudentInfo() {
+		return `${this.name} lives in ${this.city} and studies in ${this.unv} and has the student id of ${this.id}`;
+	}
+}
+
+let newStudent = new Student('Akshay', '3', 'Hyd', 'University of Illunoi');
+console.log(newStudent.showStudentInfo());
+let newLibrarian = new Librarian('Jane', 2, 'Hyd');
+console.log(newLibrarian.showInfo());
