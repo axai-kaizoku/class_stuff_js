61f5caab7945013897e47826cc9baa34a6c726aa
axai <02b3akshay@gmail.com>
2023-08-20 20:31:24 +0530

    Add img gallery activity but incomplete

--- test.js
+++ test.js
@@ -1,32 +1,26 @@
-// Define the post content
-let post = `
-<div class="post">
-    <img
-        src="https://picsum.photos/400/300"
-        alt="picsum pic" />
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-    <button id="likeBtn" style="cursor: pointer">Like</button>
-		<span id="likeCount">0</span> Likes
-</div>`;
+const images = document.querySelectorAll('.images');
 
-// // create a random number of posts
-// let randNum = Math.floor(2 + Math.random() * 4);
-// console.log(randNum);
-// for (let i = 0; i < randNum; i++) {
-// 	post += post;
-// 	console.log(post);
-// 	console.log(randNum);
-// }
+images.forEach((image, index) => {
+	image.addEventListener('click', () => {
+		const overlay = document.getElementById('productOverlay');
+		const overlayImage = document.getElementById('productImage');
+		const productName = document.getElementById('productName');
 
-// Get the postsDiv and set its innerHTML
-const postsDiv = document.getElementById('posts');
-postsDiv.innerHTML = post;
+		// Sample data. Ideally, you'd fetch this from a server or have it predefined
+		const products = [
+			{ name: 'Product 1', image: '//picsum.photos/id/12/500' },
+			{ name: 'Product 2', image: '//picsum.photos/id/80/500' },
+			// ... add more products data here
+		];
 
-const likeBtn = document.getElementById('likeBtn');
-const likeCountSpan = document.getElementById('likeCount');
-let count = 0;
-
-likeBtn.addEventListener('click', () => {
-	count += 1;
-	likeCountSpan.innerText = count; // Update the count in the span
+		// Update overlay content based on clicked image
+		productName.textContent = products[index].name;
+		overlayImage.src = products[index].image;
+		overlay.style.display = 'block';
+	});
 });
+
+function closeOverlay() {
+	const overlay = document.getElementById('productOverlay');
+	overlay.style.display = 'none';
+}
