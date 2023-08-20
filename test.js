84c2ad87b552ef2c0df5eee7ae6ad3221cabc9e4
axai <02b3akshay@gmail.com>
2023-08-20 21:06:03 +0530

    Add closures-0

--- test.js
+++ test.js
@@ -1,26 +1,19 @@
-function compute(x, y, fn) {
-	if (typeof x === 'number' && typeof y === 'number') {
-		return () => fn(x, y);
-	}
-	return () => null;
+function close() {
+	let count = 0;
+	return () => {
+		count++;
+		return count;
+	};
 }
 
-function multiply(x, y) {
-	return x * y;
-}
+let counter = close();
 
-function add(x, y) {
-	return x + y;
-}
-
-function substract(a, b) {
-	return a - b;
-}
-
-let sub = compute(40, 20, substract);
-let multpy = compute(10, 20, multiply);
-let ad = compute(10, 20, add);
-
-console.log(sub());
-console.log(ad());
-console.log(multpy());
+console.log(close()());
+console.log(`-------`);
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
