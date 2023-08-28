f05b3c7d5e8dca8388926704e5b4b5406fa55389
axai <02b3akshay@gmail.com>
2023-08-28 12:45:33 +0530

    Add an API activity (using fetch, async, await)

--- projectAPI/script.js
+++ projectAPI/script.js
@@ -1,53 +1,68 @@
-// https://www.googleapis.com/books/v1/volumes?q=quilting
-
 let searchButton = document.getElementById('searchButton');
 searchButton.addEventListener('click', fetchBooks);
 
 let allBooks = document.getElementById('allBooks');
 let content = '';
 
-function fetchBooks() {
-	// Step 1 - Read user input
-	let bookTitle = document.getElementById('bookTitle').value;
-	// console.log(bookTitle);
-	document.getElementById('bookTitle').value = '';
-
-	// Step 2 - Create the request
-	let myRequest = new XMLHttpRequest(); // readystate = 0
-	myRequest.open(
-		'GET',
-		`https://www.googleapis.com/books/v1/volumes?q=${bookTitle}`,
-	); // readystate = 1
-
-	// Step 3 - Send the request and receive response
-	myRequest.send(); // readystate = 2
-
-	// server processing, readystate = 3
-	// after response, readystate = 4
-	myRequest.onreadystatechange = () => {
-		if (myRequest.readyState === 4 && myRequest.status === 200) {
-			let convertedBooks = JSON.parse(myRequest.response);
-			let newBook = convertedBooks.items
-				.map((book) => {
-					let imgUrl =
-						book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.thumbnail
-							? book.volumeInfo.imageLinks.thumbnail
-							: ''; // Provide a default image path if you have one
-					return `
-                  <div class="book">
-                      <img src="${imgUrl}" alt="${book.volumeInfo.title}"/>
-                      <h2>${book.volumeInfo.title}</h2>
-                      <p>${book.volumeInfo.description}</p>
-                  </div>`;
-				})
-				.join('');
-			content += newBook;
-		}
+async function fetchBooks() {
+	try {
+		content = '';
 		allBooks.innerHTML = content;
-	};
-	content = '';
-	allBooks.innerHTML = content;
-	// Step 4  - Convert response to JS object
 
-	// Step 5 - Display on the front end
+		// Step 1 - Read user input
+		let bookTitle = document.getElementById('bookTitle').value;
+		document.getElementById('bookTitle').value = '';
+
+		// Step 2 & 3 - Create the request, Send the request and receive response
+		let response = await fetch(
+			`https://www.googleapis.com/books/v1/volumes?q=${bookTitle}`,
+		);
+
+		// Check if the response is ok (status 200-299)
+		if (!response.ok) {
+			throw new Error('Network response was not ok');
+		}
+
+		// Step 4 - Convert response to JS object
+		let convertedBooks = await response.json();
+
+		// Step 5 - Display on the front end
+		let newBook = convertedBooks.items
+			.map((book) => {
+				let imgUrl =
+					book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.thumbnail
+						? book.volumeInfo.imageLinks.thumbnail // 127.99 * 200
+						: 'https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-1-scaled-1150x647.png'; // If you have a default image
+				let description = book.volumeInfo.description;
+
+				// Trim the description to 50 words
+				if (description) {
+					description = description.split(' ').slice(0, 50).join(' ');
+					if (book.volumeInfo.description.split(' ').length > 50) {
+						description += '...';
+					}
+				}
+
+				if (description === undefined) {
+					description = 'No description found';
+				}
+
+				return `
+                <div class="book">
+                    <img src="${imgUrl}" alt="${book.volumeInfo.title}" id="image"/>
+                    <h2>${book.volumeInfo.title}</h2>
+                    <p>${description}</p>
+                </div>`;
+			})
+			.join('');
+
+		content += newBook;
+		allBooks.innerHTML = content;
+	} catch (error) {
+		allBooks.innerHTML = `<h1>Book not found or some error occurred.</h1>`;
+		console.error(
+			'There was a problem with the fetch operation:',
+			error.message,
+		);
+	}
 }
