fe4e6261b642736174731545a39feea06f31cdad
axai <02b3akshay@gmail.com>
2023-09-02 22:44:11 +0530

    foot ball poster activity

--- React/demo-app/src/App.js
+++ React/demo-app/src/App.js
@@ -1,12 +1,20 @@
-import WelcomeMessage from './Components/WelcomeMessage/index';
 import './App.css';
+import Images from './Components/images';
+import Heading from './Components/heading';
+import TeamName from './Components/teamname';
 
 function App() {
 	return (
 		<div className="App">
-			<WelcomeMessage timing="morning" />
-			<WelcomeMessage timing="evening" />
-			<WelcomeMessage timing="night" />
+			<Heading />
+			<Images
+				team_1="https://m.media-amazon.com/images/I/41a7y+YD9yL.jpg"
+				team_2="https://wallpaperaccess.com/thumb/910036.jpg"
+			/>
+			<TeamName
+				team_1="MANCHESTER UNITED"
+				team_2="CHELSEA"
+			/>
 		</div>
 	);
 }
