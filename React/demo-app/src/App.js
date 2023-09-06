efc9592a608f7d56b0644596e57362ac695952d6
axai <02b3akshay@gmail.com>
2023-09-06 15:20:34 +0530

    Add shopping cart

--- React/demo-app/src/App.js
+++ React/demo-app/src/App.js
@@ -1,39 +1,34 @@
-import { Component } from 'react';
+import React from 'react';
+import CartItems from './Data';
+import Item from './Item';
 
-class App extends Component {
-	state = {
-		switch: 'on',
-		image:
-			'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8WY_Yh3PcEmZDaqUyruDCeQX-qsxgV8TSyY7ExdYu_3eCBocS5EO1YqfCpJImcD4CLA4&usqp=CAU',
-	};
-	bulbON = () => {
-		this.setState({
-			switch: 'on',
-			image:
-				'https://e7.pngegg.com/pngimages/33/977/png-clipart-incandescent-light-bulb-lighting-transparency-and-translucency-bulb-light-fixture-lamp-thumbnail.png',
+const App = () => {
+	const calculateCartTotal = () => {
+		let total = 0;
+		CartItems.forEach((item) => {
+			total += item.price * item.quantity;
 		});
+		return total;
 	};
-	bulbOFF = () => {
-		this.setState({
-			switch: 'off',
-			image:
-				'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8WY_Yh3PcEmZDaqUyruDCeQX-qsxgV8TSyY7ExdYu_3eCBocS5EO1YqfCpJImcD4CLA4&usqp=CAU',
-		});
-	};
-	render() {
-		return (
-			<div className="container">
-				<img
-					alt="piscum-random"
-					src={this.state.image}
-				/>
-				<div className="buttons">
-					<button onClick={this.bulbON}>ON</button>
-					<button onClick={this.bulbOFF}>OFF</button>
-				</div>
+
+	return (
+		<div className="container">
+			<h1>Shopping Cart</h1>
+			<div className="items">
+				{CartItems.map((e, i) => (
+					<>
+						<Item
+							product={e}
+							key={e.id}
+							qp={e.price * e.quantity}
+						/>
+					</>
+				))}
 			</div>
-		);
-	}
-}
+
+			<h2>{calculateCartTotal()}</h2>
+		</div>
+	);
+};
 
 export default App;
