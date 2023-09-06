0134f367a06d85f79b66f2bdb0557c58393900a4
axai <02b3akshay@gmail.com>
2023-09-06 09:16:01 +0530

    Add blub light ON & OFF (but suitable bulb not found)

--- React/demo-app/src/App.js
+++ React/demo-app/src/App.js
@@ -1,21 +1,39 @@
-import './App.css';
-import Employee from './Components/Employee';
-import employeeData from './Sources/EmpData';
+import { Component } from 'react';
 
-function App() {
-	return (
-		<div className="bg-container">
-			<div className="heading">Employee List</div>
-			<div className="emp-list">
-				{employeeData.map((e, i) => (
-					<Employee
-						emp={e}
-						key={e.id}
-					/>
-				))}
+class App extends Component {
+	state = {
+		switch: 'on',
+		image:
+			'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8WY_Yh3PcEmZDaqUyruDCeQX-qsxgV8TSyY7ExdYu_3eCBocS5EO1YqfCpJImcD4CLA4&usqp=CAU',
+	};
+	bulbON = () => {
+		this.setState({
+			switch: 'on',
+			image:
+				'https://e7.pngegg.com/pngimages/33/977/png-clipart-incandescent-light-bulb-lighting-transparency-and-translucency-bulb-light-fixture-lamp-thumbnail.png',
+		});
+	};
+	bulbOFF = () => {
+		this.setState({
+			switch: 'off',
+			image:
+				'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8WY_Yh3PcEmZDaqUyruDCeQX-qsxgV8TSyY7ExdYu_3eCBocS5EO1YqfCpJImcD4CLA4&usqp=CAU',
+		});
+	};
+	render() {
+		return (
+			<div className="container">
+				<img
+					alt="piscum-random"
+					src={this.state.image}
+				/>
+				<div className="buttons">
+					<button onClick={this.bulbON}>ON</button>
+					<button onClick={this.bulbOFF}>OFF</button>
+				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
 
 export default App;
