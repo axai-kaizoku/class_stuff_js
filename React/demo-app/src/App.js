61e955f2001f4e77bac0e2cefefd7b67a29050d4
axai <02b3akshay@gmail.com>
2023-09-13 23:09:25 +0530

    Add Higher Order Components

--- React/demo-app/src/App.js
+++ React/demo-app/src/App.js
@@ -1,21 +1,12 @@
 import React from 'react';
-import Input from './Components/Input';
+import UsersList from './UsersList';
 
 class App extends React.Component {
 	render() {
 		return (
 			<>
 				<div className="app-container">
-					<Input render={(value) => <h2>{value}</h2>} />
-					<Input
-						type="number"
-						render={(value) => <p>{value * 2}</p>}
-					/>
-					<Input
-						render={(value) => (
-							<p style={{ color: 'red' }}>{value.toUpperCase()}</p>
-						)}
-					/>
+					<UsersList />
 				</div>
 			</>
 		);
