e938510441108fd02c08f753da5bc1cd0497bafd
axai <02b3akshay@gmail.com>
2023-09-15 12:55:59 +0530

    Add useState hook

--- React/demo-app/src/App.js
+++ React/demo-app/src/App.js
@@ -1,16 +1,46 @@
-import React from 'react';
-import UsersList from './UsersList';
+import React, { useEffect, useState } from 'react';
 
-class App extends React.Component {
-	render() {
-		return (
-			<>
-				<div className="app-container">
-					<UsersList />
-				</div>
-			</>
-		);
-	}
-}
+const App = () => {
+	const [count, setCount] = useState(0);
+	const [name, setName] = useState('');
+	// console.log(count);
+	useEffect(() => {
+		// console.log('useEffect called');
+	}, [name]);
+	return (
+		<div>
+			<h1>Count : {count}</h1>
+
+			<br />
+			<button
+				onClick={() => {
+					setCount(count + 1);
+					// console.log(count);
+				}}>
+				+1
+			</button>
+			<br />
+			<button
+				onClick={() => {
+					setCount(count - 1);
+					// console.log(count);
+				}}>
+				{' '}
+				- 1{' '}
+			</button>
+			<hr />
+			<input
+				value={name}
+				// ref={inputName}
+				// onKeyUp={(e) => {
+				// 	e.keyCode == 13 && setName(e.target.value);
+				// 	console.log(e.target.value);
+				// }}
+				onChange={(e) => setName(e.target.value)}
+			/>
+			<h2>{name}</h2>
+		</div>
+	);
+};
 
 export default App;
