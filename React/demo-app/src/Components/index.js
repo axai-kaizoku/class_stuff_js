c7d7d038475a6e1651eed3788a28264471a8609d
axai <02b3akshay@gmail.com>
2023-09-17 10:01:45 +0530

    Add world tour api -2

--- React/demo-app/src/Components/index.js
+++ React/demo-app/src/Components/index.js
@@ -4,15 +4,17 @@ import './index.css';
 const Country = ({ details }) => {
 	return (
 		<div className="country">
-			<img
-				src={details.flags.png}
-				alt="picsum"
-			/>
+			<div className="img">
+				<img
+					src={details.flags.png}
+					alt="picsum"
+				/>
+			</div>
 			<div className="details">
 				<h2>{details.name.common}</h2>
-				<h4>Official Name : {details.name.official}</h4>
-				<h4>capital : {details.capital}</h4>
-				<h4>region : {details.region}</h4>
+				<h4>Official Name: {details.name.official}</h4>
+				<h4>Capital: {details.capital}</h4>
+				<h4>Region: {details.region}</h4>
 			</div>
 		</div>
 	);
