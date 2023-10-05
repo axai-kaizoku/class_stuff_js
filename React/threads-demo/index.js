3bce257d6253175d461bace6eb96d4edcf679578
axai <02b3akshay@gmail.com>
2023-10-05 18:45:56 +0530

    Add multithreading workers running

--- React/threads-demo/index.js
+++ React/threads-demo/index.js
@@ -1,16 +1,23 @@
-const { parentPort } = require('worker_threads');
+const { Worker, isMainThread, parentPort } = require('worker_threads');
 
-function performTask() {
-	let count = 0;
-	while (count < 10) {
-		console.log(`Worker : Processing task ${count}`);
-		count++;
-	}
+if (isMainThread) {
+	const worker = new Worker(__filename);
+
+	worker.on('message', (message) => {
+		console.log(`Result of computation : ${message}`);
+		process.exit(0);
+	});
+
+	worker.postMessage('Starting Computation... please wait...');
+} else {
+	parentPort.on('message', (message) => {
+		console.log('Worker says : ', message);
+
+		let result = 0;
+		for (let i = 0; i < 1000000000; i++) {
+			result += i;
+		}
+
+		parentPort.postMessage(result);
+	});
 }
-
-function notifyTaskComplete() {
-	parentPort.postMessage('Task Completed!');
-}
-
-performTask();
-notifyTaskComplete();
