ef6b9079587860499e46b636536188aecd20e51c
axai <02b3akshay@gmail.com>
2023-10-15 16:20:51 +0530

    Add navigation pages (routes) activity (chatgpt version)

--- code_activities/express-activities/app.js
+++ code_activities/express-activities/app.js
@@ -1,32 +1,45 @@
-import bodyParser from 'body-parser';
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
 	res.render('home');
 });
 
-app.get('/projects', (req, res) => {
-	res.render('projects');
+app.post('/posts', (req, res) => {
+	const { post } = req.body;
+	// Store post in database (for simplicity, not implemented here)
+	res.status(201).json({ message: 'Post shared successfully!', post });
 });
 
-app.get('/about', (req, res) => {
-	res.render('about');
+app.get('/users/:userId/posts', (req, res) => {
+	const userId = req.params.userId;
+	// Fetch posts from the database by userId (for simplicity, not implemented here)
+	res.json({ posts: [] });
 });
 
-app.get('/contact', (req, res) => {
-	res.render('contact');
+app.post('/users/:userId/follow', (req, res) => {
+	const userIdToFollow = req.params.userId;
+	// Implement logic to let the current user follow userIdToFollow
+	res
+		.status(200)
+		.json({ message: `Successfully followed user with ID ${userIdToFollow}` });
 });
 
-app.post('/feedback', (req, res) => {
-	const feedback = req.body.feedback;
-	console.log('Received feedback: ', feedback);
+app.post('/posts/:postId/like', (req, res) => {
+	const postId = req.params.postId;
+	// Implement logic to like the post with postId
+	res
+		.status(200)
+		.json({ message: `Successfully liked post with ID ${postId}` });
+});
 
-	res.send('Thankyou for your feedback');
+app.use((req, res, next) => {
+	res.status(404).json({ error: 'Resource not found' });
 });
 
 app.listen(3000, () => {
