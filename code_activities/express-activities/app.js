0f680d038bbbcb1a0fef994b41d3499bd1597980
axai <02b3akshay@gmail.com>
2023-10-15 18:40:57 +0530

    Add rendering dynamic pug files activity

--- code_activities/express-activities/app.js
+++ code_activities/express-activities/app.js
@@ -1,45 +1,62 @@
 import express from 'express';
-import bodyParser from 'body-parser';
+import { join } from 'path';
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'pug');
+app.use('/assets', express.static(join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-	res.render('home');
+	res.render('home', {
+		posts: [
+			{
+				id: 1,
+				user: 'Axai Y',
+				image: '//picsum.photos/id/237/300/300',
+				content:
+					'The express frame work is more convenient to use and not strict to libraries',
+			},
+			{
+				id: 2,
+				user: 'Carry Minati',
+				image: '//picsum.photos/id/77/300/300',
+				content: "It's a great templating language and very easy to use!",
+			},
+			{
+				id: 3,
+				user: 'Jon Snow',
+				image: '//picsum.photos/id/74/300/300',
+				content: 'Winter is coming Bruh!',
+			},
+			{
+				id: 4,
+				user: 'Alice Tower',
+				image: '//picsum.photos/id/64/300/300',
+				content: 'Night is still young. #night #hightower',
+			},
+			{
+				id: 5,
+				user: 'Scott Mime',
+				image: '//picsum.photos/id/24/300/300',
+				content: 'Northern places are very good to visit in winter.',
+			},
+			{
+				id: 6,
+				user: 'Larry wheels',
+				image: '//picsum.photos/id/12/300/300',
+				content: 'Power Quality and Facts is a good subject',
+			},
+		],
+	});
 });
 
-app.post('/posts', (req, res) => {
-	const { post } = req.body;
-	// Store post in database (for simplicity, not implemented here)
-	res.status(201).json({ message: 'Post shared successfully!', post });
-});
-
-app.get('/users/:userId/posts', (req, res) => {
-	const userId = req.params.userId;
-	// Fetch posts from the database by userId (for simplicity, not implemented here)
-	res.json({ posts: [] });
-});
-
-app.post('/users/:userId/follow', (req, res) => {
-	const userIdToFollow = req.params.userId;
-	// Implement logic to let the current user follow userIdToFollow
-	res
-		.status(200)
-		.json({ message: `Successfully followed user with ID ${userIdToFollow}` });
-});
-
-app.post('/posts/:postId/like', (req, res) => {
-	const postId = req.params.postId;
-	// Implement logic to like the post with postId
-	res
-		.status(200)
-		.json({ message: `Successfully liked post with ID ${postId}` });
+app.get('/create-post', (req, res) => {
+	res.render('createPost');
 });
 
 app.use((req, res, next) => {
-	res.status(404).json({ error: 'Resource not found' });
+	res.render('error');
 });
 
 app.listen(3000, () => {
